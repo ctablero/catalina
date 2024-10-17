40c28d72df5ca2335281c7e9d6b6ad1cdede0f04
Cesar Tablero <cesarjthernandez@gmail.com>
2024-10-16 22:09:55 -0400

    catalina-6: First split the main run db server script in subscripts per db client to use.

--- run_db.sh
+++ run_db.sh
@@ -1,3 +1,4 @@
 
 echo "Starting database server..."
-docker run -d -p "$DB_HOST_PORT:$DB_INTERNAL_PORT" --name "$DB_HOST" --network "$DB_NETWORK" -e MYSQL_ROOT_PASSWORD="$DB_PASSWORD" "$DB_IMAGE_NAME:$DB_IMAGE_TAG"
+
+./mysql.sh
