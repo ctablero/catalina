5010f3a06b8ce29561ced0f0e4b847798df198f9
Cesar Tablero <cesarjthernandez@gmail.com>
2025-01-15 22:25:22 -0400

    catalina-12: add script to run mssql server and validations

--- run_db.sh
+++ run_db.sh
@@ -3,4 +3,9 @@ echo "Starting database server..."
 
 if [ "$RDBMS" = 'mysql' ]; then 
     ./mysql.sh
+elif [ "$RDBMS" = 'mssql' ]; then
+    ./mssql.sh
+else
+    echo "Unknown RDBMS"
+    exit 1
 fi
