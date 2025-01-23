d91fa04fa9aec007ecdf00722e278c0d24daff8e
Cesar Tablero <cesarjthernandez@gmail.com>
2025-01-22 21:43:43 -0400

    catalina-12: Create Dockerfile for MSSQL

--- knex/knexfile.js
+++ knex/knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
         client: process.env.DB_CLIENT,
         connection: {
             host: process.env.DB_HOST,
-            port: process.env.DB_HOST_PORT,
+            port: 1433,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
--- mssql.sh
+++ mssql.sh
@@ -1,4 +1,7 @@
 echo "Starting mssql server..."
-docker run -e 'ACCEPT_EULA=Y' -e "'SA_PASSWORD=$DB_PASSWORD'" \
+docker run -e 'ACCEPT_EULA=Y' -e MSSQL_SA_PASSWORD="$DB_PASSWORD" \
  -p "$DB_HOST_PORT":"$DB_INTERNAL_PORT" --name "$DB_HOST" \
  --network "$DB_NETWORK" -d "$DB_IMAGE_NAME":"$DB_IMAGE_TAG"
+
+echo printing environment vsariables inside the container
+docker exec "$DB_HOST" printenv 
