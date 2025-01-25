f5e6a868f093db158c3105b6c503077f0568e84f
Cesar Tablero <cesarjthernandez@gmail.com>
2025-01-24 23:59:06 -0400

    catalina-12: Cast port to integer in knexfile. Reduced inputs and made more default values.

--- knex/knexfile.js
+++ knex/knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
         client: process.env.DB_CLIENT,
         connection: {
             host: process.env.DB_HOST,
-            port: 1433,
+            port: parseInt(process.env.DB_HOST_PORT),
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
