f84cae749a584a38615317ac85c4c82b91cc2427
Cesar Tablero <cesarjthernandez@gmail.com>
2025-01-25 00:48:46 -0400

    catalina-12: Added mysql custom db

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,5 @@
 
 echo "Starting mysql server..."
-docker run -d -p "$DB_HOST_PORT:$DB_INTERNAL_PORT" --name "$DB_HOST" --network "$DB_NETWORK" -e MYSQL_ROOT_PASSWORD="$DB_PASSWORD" -e MYSQL_DATABASE="$DB_NAME" "$DB_IMAGE_NAME:$DB_IMAGE_TAG"
+docker run -d -p "$DB_HOST_PORT:$DB_INTERNAL_PORT" --name "$DB_HOST" \
+    --network "$DB_NETWORK" -e MYSQL_ROOT_PASSWORD="$DB_PASSWORD" -e MYSQL_DATABASE="$DB_NAME" \
+    "$DB_IMAGE_NAME:$DB_IMAGE_TAG"
