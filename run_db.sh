7ce4a4dd6c95e205ed70b0c909d4ddbcdfc057b9
Cesar Tablero <cesarjthernandez@gmail.com>
2025-01-30 21:26:24 -0400

    catalina-16: Add postgresql support

--- run_db.sh
+++ run_db.sh
@@ -5,6 +5,8 @@ if [ "$RDBMS" = 'mysql' ]; then
     ./mysql.sh
 elif [ "$RDBMS" = 'mssql' ]; then
     ./mssql.sh
+elif [ "$RDBMS" = 'postgres' ]; then
+    ./postgres.sh
 else
     echo "Unknown RDBMS"
     exit 1
